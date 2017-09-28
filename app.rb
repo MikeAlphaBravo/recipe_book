3195e89a37eb3ac42f47e917579c9434a03e9b31
Michael Brooks and Mark Helt <michael@>
2017-09-28 11:07:20 -0700

    fill out routes and views fully

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-
+Recipe.create({:name => 'test recipe'})
+Ingredient.create({:item => 'butter'})
+Tag.create({:category => 'italian'})
 
 get('/')do
   @tags = Tag.all
@@ -12,20 +14,39 @@ get('/')do
 end
 
 get('/category/create') do
+  @recipes = Recipe.all
   erb(:create_category)
 end
 
 # post('/category/create') do
 #   recipe_ids = params['recipe_ids']
-#   Tag.create({:recipe_ids =>recipe_ids})
+#   Tag.new({:recipe_ids => recipe_ids})
 #   binding.pry
 #   redirect('/categories')
 # end
 
 get('/recipe/create') do
+  @tags = Tag.all
+  @ingredients = Ingredient.all
   erb(:create_recipe)
 end
 
 get('/ingredient/create') do
+  @recipes = Recipe.all
   erb(:create_ingredient)
+end
+
+get('/categories') do
+  @tags = Tag.all
+  erb(:categories)
+end
+
+get('/recipes') do
+  @recipes = Recipe.all
+  erb(:recipes)
+end
+
+get('/ingredients') do
+  @ingredients = Ingredient.all
+  erb(:ingredients)
 end
