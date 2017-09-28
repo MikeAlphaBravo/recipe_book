bf4886f80e1467e929580d6fadbb7016b32aae61
Michael Brooks and Mark Helt <mark@>
2017-09-28 12:01:21 -0700

    add delte functionality and ingredient creation

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require("bundler/setup")
+require('pry')
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
@@ -12,30 +13,65 @@ get('/')do
   @ingredients = Ingredient.all
   erb(:index)
 end
-
+##########################################################################
 get('/category/create') do
   @recipes = Recipe.all
   erb(:create_category)
 end
 
-# post('/category/create') do
-#   recipe_ids = params['recipe_ids']
-#   Tag.new({:recipe_ids => recipe_ids})
-#   binding.pry
-#   redirect('/categories')
-# end
+post('/category/create') do
+  recipe_ids = params['recipe_ids']
+  Tag.new({:recipe_ids => recipe_ids})
+  binding.pry
+  redirect('/categories')
+end
 
+get ('/category/delete/:id') do
+  Tag.find(params['id']).destroy
+  redirect('/categories')
+end
+
+# get('/category/edit/:id') do
+#   Tag.find(params['id'])
+#   erb(:)
+# end
+##########################################################################
 get('/recipe/create') do
   @tags = Tag.all
   @ingredients = Ingredient.all
   erb(:create_recipe)
 end
 
+post('/recipe/create') do
+  tag_ids = params['tag_ids']
+  name = params['name']
+  Recipe.create({:name => name, :tag_ids => tag_ids})
+  binding.pry
+  redirect('/recipes')
+end
+
+get ('/recipe/delete/:id') do
+  Recipe.find(params['id']).destroy
+  redirect('/recipes')
+end
+##########################################################################
 get('/ingredient/create') do
   @recipes = Recipe.all
   erb(:create_ingredient)
 end
 
+post('/ingredient/create') do
+  item = params['item']
+  recipe_ids = params['recipe_ids']
+  Ingredient.create({:item => item, :recipe_ids => recipe_ids})
+  redirect('/ingredients')
+end
+
+get ('/ingredient/delete/:id') do
+  Ingredient.find(params['id']).destroy
+  redirect('/ingredients')
+end
+##########################################################################
 get('/categories') do
   @tags = Tag.all
   erb(:categories)
