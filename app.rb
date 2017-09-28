22198406b48befb7f6f33da2cbb249c5e27e7046
Michael Brooks and Mark Helt <michael@>
2017-09-27 17:02:11 -0700

    spec backend and rout create pages

--- app.rb
+++ app.rb
@@ -1,3 +1,23 @@
 require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+get('/')do
+  @tags = Tag.all
+  @recipes = Recipe.all
+  @ingredients = Ingredient.all
+  erb(:index)
+end
+
+get('/category/create') do
+  erb(:create_category)
+end
+
+get('/recipe/create') do
+  erb(:create_recipe)
+end
+
+get('/ingredient/create') do
+  erb(:create_ingredient)
+end
